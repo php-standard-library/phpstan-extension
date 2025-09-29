13d9f416ac14eca31f6b4c75960b770eeebda126
Pablo Largo Mohedano <devnix.code@gmail.com>
2025-09-29 16:51:42 +0200

    More precise types after `Option::filter()`

---
+++ tests/Option/PslTypeSpecifyingExtensionTest.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types = 1);
+
+namespace Psl\PHPStan\Option;
+
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
+use PHPStan\Testing\TypeInferenceTestCase;
+use PHPUnit\Framework\Assert;
+use function sprintf;
+
+class PslTypeSpecifyingExtensionTest extends TypeInferenceTestCase
+{
+
+	/**
+	 * @return iterable<mixed>
+	 */
+	public function dataFileAsserts(): iterable
+	{
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/filter.php');
+	}
+
+	/**
+	 * @dataProvider dataFileAsserts
+	 * @param mixed ...$args
+	 */
+	public function testFileAsserts(
+		string $assertType,
+		string $file,
+		...$args
+	): void
+	{
+		if (!InstalledVersions::satisfies(new VersionParser(), 'azjezz/psl', '>=2.2.0')) {
+			Assert::markTestSkipped(sprintf('Option component is not available in current azjezz/psl installed version'));
+		}
+
+		$this->assertFileAsserts($assertType, $file, ...$args);
+	}
+
+	/***
+	 * @return string[]
+	 */
+	public static function getAdditionalConfigFiles(): array
+	{
+		return [__DIR__ . '/../../extension.neon'];
+	}
+
+}
