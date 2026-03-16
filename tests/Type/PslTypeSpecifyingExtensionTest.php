95799bdd3d0f0337f1d5c28c73c0f413c4f9baec
Toon Verwerft <toonverwerft@gmail.com>
2026-03-16 20:51:59 +0100

    Use NullishType::class and fix CS violations
    - Restore NullishType::class instead of string literal
    - Add baseline ignores for older PSL versions without NullishType
    - Fix CS: add use statements for NullishType and class_exists in test

--- tests/Type/PslTypeSpecifyingExtensionTest.php
+++ tests/Type/PslTypeSpecifyingExtensionTest.php
@@ -5,6 +5,8 @@ namespace Psl\PHPStan\Type;
 use Composer\InstalledVersions;
 use Composer\Semver\VersionParser;
 use PHPStan\Testing\TypeInferenceTestCase;
+use Psl\Type\Internal\NullishType;
+use function class_exists;
 
 class PslTypeSpecifyingExtensionTest extends TypeInferenceTestCase
 {
@@ -17,7 +19,7 @@ class PslTypeSpecifyingExtensionTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/coerce.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/assert.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/matches.php');
-		if (class_exists(\Psl\Type\Internal\NullishType::class)) {
+		if (class_exists(NullishType::class)) {
 			yield from $this->gatherAssertTypes(__DIR__ . '/data/nullishCoerce.php');
 			yield from $this->gatherAssertTypes(__DIR__ . '/data/nullishAssert.php');
 			yield from $this->gatherAssertTypes(__DIR__ . '/data/nullishMatches.php');
